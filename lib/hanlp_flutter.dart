e75130095fff0ceb71272b9687b5cc81bab3cb2f
MHeaven <331017613@qq.com>
2022-12-29 17:05:21 +0800

    1

--- lib/hanlp_flutter.dart
+++ lib/hanlp_flutter.dart
@@ -1,3 +1,7 @@
+//这里声明一下library, flutter_native_hlper就是项目名称
+library hanlp_flutter;
+//下面这两个，就是要对外公开的类（根据自己项目的需要来公开）
+export 'package:hanlp_flutter/hanlp_flutter.dart';
 
 import 'hanlp_flutter_platform_interface.dart';
 
