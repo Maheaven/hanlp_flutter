b473af034840f3c4f2ad43fbc9a27f1075277b2c
MHeaven <331017613@qq.com>
2022-12-29 17:17:09 +0800

    1

--- lib/hanlp_flutter.dart
+++ lib/hanlp_flutter.dart
@@ -7,11 +7,11 @@ import 'hanlp_flutter_platform_interface.dart';
 
 class HanlpFlutter {
 
-  Future<String?> getPlatformVersion() {
+  static Future<String?> getPlatformVersion() {
     return HanlpFlutterPlatform.instance.getPlatformVersion();
   }
 
-  Future<String?> hans2hant(String text) {
+  static Future<String?> hans2hant(String text) {
     return HanlpFlutterPlatform.instance.hans2hant(text);
   }
 
